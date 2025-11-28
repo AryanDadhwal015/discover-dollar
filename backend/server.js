b9ab9190bbc25e2911ac498e55f9acab3c5a6f7b
Ubuntu <ubuntu@ip-172-31-12-4.ap-south-1.compute.internal>
2025-11-28 05:20:50 +0000

    final-updates

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
-const app = express();
+const app = express();      // ✅ define app first
+app.use(cors());             // ✅ then enable CORS
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -28,10 +29,12 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
 
+// routes
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
